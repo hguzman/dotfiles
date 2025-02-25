07656911d45851efb33b8b98d28355be0695c187
Henry Guzman <hguzman@cars-ok.com>
2025-02-25 17:04:02 -0500

    Snnipet

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -2,6 +2,12 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
+	    -- Agrega la Layer a RUBYLIB para que Solargraph pueda encontrarla
+	    -- vim.env.RUBYLIB = vim.fn.expand("~/develop/ruby/aws_sam/electoral-sam/layers/core/ruby/lib/") .. ":" .. (vim.env.RUBYLIB or "")
+
+	    local lib_path = vim.fn.expand("~/develop/ruby/aws_sam/electoral-sam/layers/core/ruby/lib")
+	    -- Asegurar que RUBYLIB tenga la ruta de la layer
+	    vim.env.RUBYLIB = lib_path .. ":" .. (vim.env.RUBYLIB or "")
       require("lspconfig").solargraph.setup({
 	      cmd = { "solargraph", "stdio" },
 	      filetypes = { "ruby" },
@@ -9,6 +15,9 @@ return {
 	      settings = {
 		      solargraph = {
 			      diagnostics = true, -- Habilitar diagnósticos
+			      workspace = {
+				      library = { lib_path }
+			      }
 		      },
 	      },
       })
--- nvim/lua/snippets/ruby.lua
+++ nvim/lua/snippets/ruby.lua
@@ -6,3 +6,14 @@ local t = ls.text_node
 --   s("frozen1", { t("# frozen_string_literal: true") }),
 -- })
 
+ls.add_snippets("ruby", {
+  s("handler", {
+    t("# frozen_string_literal: true"),
+    t({ "", "def " }), i(1, "handler"), t("(event:, context:)"),
+    t({ "", "  # TODO: Implementar lógica de la función Lambda" }),
+    t({ "", "  { statusCode: 200, body: 'Hello from Lambda!' }" }),
+    t({ "", "end" }),
+  }),
+})
+
+
